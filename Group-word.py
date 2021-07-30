5c17ab1bbddd545fa50efc4b69cc0bb3ee06078e
HyunDel <pshyun1331@gmail.com>
2021-07-30 17:18:24 +0900

    단어 그룹핑

--- Group-word.py
+++ Group-word.py
@@ -7,6 +7,6 @@ class Solution:
         anagrams = collections.defaultdict(list)
 
         for word in strs:
-            # 정렬하여 딕셔너리에 추가
+            # 정렬하여 딕셔너리에 추가.
             anagrams[''.join(sorted(word))].append(word)
         return list(anagrams.values())
