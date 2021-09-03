5391034acf97d5e62594e386ccff6844f3fff003
HyunDel <pshyun1331@gmail.com>
2021-09-03 15:38:27 +0900

    스택 이용해서 큐 구현 풀이

---
+++ 스택을이용큐구현.py
@@ -0,0 +1,25 @@
+class MyQueue:
+
+    def __init__(self):
+        self.input = []
+        self.output =[]
+        
+
+    def push(self, x: int) -> None:
+        return  self.input.append(x)
+        
+
+    def pop(self) -> int:
+        self.peek()
+        return self.output.pop()
+        
+
+    def peek(self) -> int:
+        if not self.output:
+            while self.input :
+                self.output.append(self.input.pop())
+        return self.output[-1]
+        
+
+    def empty(self) -> bool:
+        return self.input == [] and self.output == []
