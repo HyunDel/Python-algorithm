3c830ac09f85ed1f7f7f964dab1807cd87cb4e8f
HyunDel <pshyun1331@gmail.com>
2021-09-09 17:32:41 +0900

    한줄 풀이방법 (파이썬다운 방법)

---
+++ 상위K빈도요소.py/파이썬다운풀이.py
@@ -0,0 +1,5 @@
+import collections 
+from typing import List
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        return list(zip(*collections.Counter(nums).most_common(k)))[0]
