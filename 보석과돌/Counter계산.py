3ba4126d162f5f2013d5eac996119836080024f2
HyunDel <pshyun1331@gmail.com>
2021-09-09 15:09:12 +0900

    Counter로 풀이방법

---
+++ 보석과돌/Counter계산.py
@@ -0,0 +1,10 @@
+import collections
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        freqs = collections.Counter(stones)
+        count =0 
+        
+        for char in jewels :
+            count += freqs[char]
+            
+        return count 
